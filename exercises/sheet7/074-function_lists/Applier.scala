a07edc896770885fc04867bc32dca1c426728135
philippreinig <philipp.reinig@gmail.com>
2022-06-13 21:05:09 +0200

    Added Sheet 7-4: Solution without standard library

--- exercises/sheet7/074-function_lists/Applier.scala
+++ exercises/sheet7/074-function_lists/Applier.scala
@@ -1,5 +1,11 @@
 object Applier {
 
+    def applyFuncsOwnRec(fList: List[Double => Double], arg: Double) : Double = {
+        if (fList.isEmpty) arg
+        else applyFuncsOwnRec(fList.tail, fList(0)(arg))
+
+    }
+
     def applyFuncsV1(fList: List[Double => Double], arg: Double): Double = {
         if (fList.isEmpty) arg
         else fList.foldLeft(arg){(result, f) => f(result)}
@@ -36,12 +42,15 @@ object Applier {
   def main(Args: Array[String]) = {
     val f1 = (x: Double) => 2 * x
     val f2 = (x: Double) => x+1
-    val fList = List(f1, f2)
-    println(applyFuncsV1(fList, 2))
-    println(applyFuncsV2(fList, 2))
-    println(applyFuncsV3(fList, 2))
-    println(applyFuncsReverseV1(fList, 2))
-    println(applyFuncsReverseV2(fList, 2))
-    println(applyFuncsArgs(fList, List(1,2)))
+    val f3 = (x: Double) => scala.math.pow(0.5*x, 2) - 7 
+    val fList = List(f1, f2, f3)
+    val x = 2
+    println(s"applyFuncsV1($x) = " + applyFuncsV1(fList, x))
+    println(s"applyFuncsV2($x) = " + applyFuncsV2(fList, x))
+    println(s"applyFuncsV3($x) = " + applyFuncsV3(fList, x))
+    println(s"applyFuncsOwnRec($x) = " + applyFuncsOwnRec(fList, 2))
+    println(s"applyFuncsReverseV1($x) = " + applyFuncsReverseV1(fList, 2))
+    println(s"applyFuncsReverseV2($x) = " + applyFuncsReverseV2(fList, 2))
+    println(s"applyFuncsArgs($x) = " + applyFuncsArgs(fList, List(1, 2, 3)))
   }
 }
